f1be050e501ec55791c891da3152a83a1a420c19
ibrahimshk6969 <ibrahimakshk@gmail.com>
2017-08-13 15:23:53 +0530

    deleted class files

---
+++ com/bridgelabz/util/Stats5.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin com/bridgelabz/util/Stats5.java
+ *  Execution:    java -cp bin com.bridgelabz.util.Stats5 
+ *  
+ *  Purpose: To stimulate a coin flip.
+ *
+ *  @author  Ibrahim
+ *  @version 1.0
+ *  @since   11-08-2017
+ *
+ ******************************************************************************/
+
+package com.bridgelabz.util;
+import java.lang.Integer;
+
+class Stats5
+{
+
+	public static void main(String args []);
+	{
+		
